bd2c7466fc5eac2ccaeceefd8a84a4d96d34e800
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2019-08-25 02:07:52 +0100

    Further algorithm filtering work
    Working on filtering potential forward TIR matches based on the following reverse matches
    
    
    Former-commit-id: e46d85f10ed52acd619fde8ec83197f6edea8769

--- R/packSearch.R
+++ R/packSearch.R
@@ -14,12 +14,14 @@ packSearch <- function(subSeq, Genome, mismatch = 0, element.length = c(300, 500
   # a summary of the search, with match statistics
   
   #perform initial search for TIR matches
-  TIR_Matches <- GRanges()
-  TIR_Matches <- identifyTIRMatches(TIR_Matches, subSeq, Genome, mismatch = mismatch, strand = "+")
-  TIR_Matches <- identifyTIRMatches(TIR_Matches, reverseComplement(subSeq), Genome, mismatch = mismatch, strand = "-")
+  forwardMatches <- GRanges()
+  forwardMatches <- identifyTIRMatches(forwardMatches, subSeq, Genome, mismatch = mismatch, strand = "+")
   
-  #determine potential transposable elements
-  #potentialPack <- identifyPotentialPackElements(subSeq, Genome, element.length, TIR_Matches)
+  reverseMatches <- GRanges()
+  reverseMatches <- identifyTIRMatches(reverseMatches, reverseComplement(subSeq), Genome, mismatch = mismatch, strand = "-")
+  
+  #determine potential transposable elements based on following elements
+  potentialPack <- identifyPotentialPackElements(forwardMatches, reverseMatches, subSeq, Genome, element.length)
   
   return(TIR_Matches)
 }
