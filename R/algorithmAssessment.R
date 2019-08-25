fc7b0fd98bfd6fb014cbfc63e08a081080037944
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2019-08-25 23:44:50 +0100

    Testing versus known CACTA elements
    begun testing the initial algorithm versus 10 known Arabidopsis thalania packCACTA transposons - many false positives present at this stage & only recognises exact TSDs

--- R/algorithmAssessment.R
+++ R/algorithmAssessment.R
@@ -1,18 +1,16 @@
-algorithmAssessment <- function(transposonList) {
+algorithmAssessment <- function(transposonList, Genome) {
   knownCACTA <- read.csv("Input/knownCACTA.csv", sep = ";")
-  score <- 0
+  knownCACTA <- knownCACTA[,1:8]
+  x <- data.frame(name = Genome@ranges@NAMES)
   
-  for(element in 1:length(knownCACTA)) {
-    i = which(transposonList$start == knownCACTA$start[element])
-    if(length(i) == 0) {
-      i <- 0
-    } else { #if (transposonList$end[i] == knownCACTA$end[element]) {
-      score <- score + 1
-    }
+  for(i in 1:length(knownCACTA$Chr)) {
+    knownCACTA$chrName[i] <- as.character(x$name[knownCACTA$Chr[i]])
   }
   
-  print(paste0("Correct packCACTA identified in Arabidopsis thalania: ", score))
-  print(paste0("Algorithm error rate: ", (1-(score/length(transposonList[,1])))))
+  knownCACTA$Identified <- knownCACTA$start %in% transposonList$start
+  
+  print(paste0("Correct packCACTA identified in Arabidopsis thalania: ", sum(knownCACTA$Identified)))
+  print(paste0("Algorithm error rate: ", (1-(sum(knownCACTA$Identified)/length(transposonList[,1])))))
 }
 
-algorithmAssessment(potentialPacks)
+algorithmAssessment(potentialPacks, Genome)
