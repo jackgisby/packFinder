1d7646ac1e082e7cea651f72a497cf64f3f8e3a4
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2019-08-28 12:52:13 +0100

    TSD matching efficiency
    Improved algorithm efficiency by using dplyr filtering instead of for loops to filter through nearby opposing TIRs - still at 0.375 error rate and 5/10 recongised transposons

--- R/Testing.R
+++ R/Testing.R
@@ -1,2 +1,28 @@
-x <- potentialPacks[potentialPacks$start %in% knownCACTA$start
-                    | potentialPacks$end %in% knownCACTA$end,]
+forwardMatches <- as.data.frame(GRanges()) %?%
+  identifyTIRMatches(forwardMatches, subSeq, Genome, mismatch = 2, strand = "+") %>%
+  getTSDs(forwardMatches, Genome, direction = "+") %>%
+  mutate(forwardMatches, TSD = mapply(function(seqnames, start, TSD.length, Genome) {
+    return(as.character(Genome[Genome@ranges@NAMES == seqnames][[1]][(end + 1):(end + TSD.length)]))},
+    seqnames,
+    start,
+    MoreArgs = list(TSD.length = TSD.length, Genome = Genome)))
+
+
+getTSDs <- function(TIR_Matches, Genome, TSD.length, direction) {
+  
+  if(direction == "+") {
+    return(TIR_Matches %>% mutate(
+      TSD = mapply(function(seqnames, start, TSD.length, Genome) {
+        return(as.character(Genome[Genome@ranges@NAMES == seqnames][[1]][(start - TSD.length):(start - 1)]))},
+        seqnames,
+        start,
+        MoreArgs = list(TSD.length = TSD.length, Genome = Genome))
+    ))
+  } else if(direction == "-") {
+    return(TIR_Matches %>% mutate(forwardMatches, TSD = mapply(function(seqnames, start, TSD.length, Genome) {
+      return(as.character(Genome[Genome@ranges@NAMES == seqnames][[1]][(end + 1):(end + TSD.length)]))},
+      seqnames,
+      start,
+      MoreArgs = list(TSD.length = TSD.length, Genome = Genome))))
+  }
+}
