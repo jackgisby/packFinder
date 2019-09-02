71d66fec95b99b24db21b6690bed3da8fe0f1a29
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-02 11:33:22 +0100

    Algorithm report function
    Introduced function to create reports based on the algorithm's performance
    
    
    Former-commit-id: abc49372e6f22df66cf8463096f78649e0cc95fc

--- R/algorithmAssessment.R
+++ R/algorithmAssessment.R
@@ -3,14 +3,11 @@ source("R/devFunctions.R")
 
 #Genome <- initialise()
 subSeq <- DNAString("CACTACAA-AAATAT") #CACTACAA-AAATAT / DNAString(consensusString(knownTIRs))
-
+max.mismatch = 2
 
 start <- Sys.time()
-potentialPacks <- packSearch(subSeq, Genome, mismatch = 2, element.length = c(300, 3500), TSD.length = 3)
+potentialPacks <- packSearch(subSeq, Genome, mismatch = max.mismatch, element.length = c(300, 3500), TSD.length = 3)
 end <- Sys.time()
 
-identifiedCACTA <- algorithmAssessment(potentialPacks, Genome)
-knownCACTA <- getArAthCACTA(Genome)
-knownTIRs <- DNAStringSet(c(knownCACTA$forwardTIR, knownCACTA$reverseTIR))
-badCACTA <- getBadMatches(knownCACTA, subSeq, 2)
+knownCACTA <- saveReport(potentialPacks, subSeq, Genome, integrityFilter = NULL, mismatch = max.mismatch)
 print(end-start)
