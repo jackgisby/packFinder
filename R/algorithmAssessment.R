b90cf6f5b38b22283ca301cdf35888028436dcb3
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-08 19:27:32 +0100

    Further blastn implementation

--- R/algorithmAssessment.R
+++ R/algorithmAssessment.R
@@ -2,20 +2,26 @@ source("R/packSearch.R")
 source("R/devFunctions.R")
 
 #pack find
-Genome <- initialise()
-subSeq <- DNAString("CACTACAA") #CACTACAA-AAATAT / DNAString(consensusString(knownTIRs))
-max.mismatch = 0
+#Genome <- initialise()
+subSeq <- DNAString("CACTACAA-AAATAT") #CACTACAA-AAATAT / DNAString(consensusString(knownTIRs))
+max.mismatch = 2
 
 start <- Sys.time()
 potentialPacks <- packSearch(subSeq, Genome, mismatch = max.mismatch, element.length = c(300, 3500), TSD.length = 3)
 end <- Sys.time()
 
+repeatMaps <- getRepeatMaps(Genome)
+potentialPacks <- filterElements(potentialPacks, repeatMaps)
+
 knownCACTA <- saveReport(potentialPacks, subSeq, Genome, integrityFilter = NULL, mismatch = max.mismatch)
+knownCACTA <- saveReport(filter(potentialPacks, isTransposon == FALSE), subSeq, Genome, integrityFilter = NULL, mismatch = max.mismatch)
 print(end-start)
 
+
+  
+#blast
 db <- blast(db="C:/Users/jackg/Documents/R/nt_db/nt/nt", type = "blastn")
 
-#blast
 start <- Sys.time()
 #BlastMatches <- packBlast(potentialPacks, "nt")
 #blastMatches <- blastSeq(DNAStringSet(Genome$`NC_003070.9 Arabidopsis thaliana chromosome 1 sequence`[13131404:13132121]),database = "nt")
@@ -24,4 +30,3 @@ blastMatch <- predict(db,
                       BLAST_args = "-num_threads 4")
 end <- Sys.time()
 print(end-start)
-
--- R/searchFunctions.R
+++ R/searchFunctions.R
@@ -100,16 +100,16 @@ identifyPotentialPackElements <- function(forwardMatches, reverseMatches, Genome
   return(potTransposons)
 }
 
-getBlastMatches <- function(potentialPacks, db) {
-  blastMatches <- list(length = length(potentialPacks))
-  if(db.loc == "online") {
+getBlastMatches <- function(potentialPacks, db, db.loc, Genome) {
+  
+  
+  if(db.loc == "local") {
+    blastMatches <- list(length = length(potentialPacks))
     for(i in 1:length(potentialPacks)) {
-      blastMatches[i] <- getBlastMatchesOnline(Genome[Genome@ranges@NAMES == potentialPacks$seqnames[i]][potentialPacks$start[i]:potentialPacks$end[i]],
-                                               db)
+      blastMatches[i] <- blastMatches[i] <- getBlastMatchesLocal(Genome[Genome@ranges@NAMES == potentialPacks$seqnames[i]][potentialPacks$start[i]:potentialPacks$end[i]],db)
     }
-  } else if(db.loc == "local") {
-    blastMatches[i] <- getBlastMatchesLocal(Genome[Genome@ranges@NAMES == potentialPacks$seqnames[i]][potentialPacks$start[i]:potentialPacks$end[i]],
-                                            db)
+  } else if(db.loc == "online") {
+    blastMatches <- getBlastMatchesOnline(potentialPacks, db, Genome)
   }
   return(blastMatches)
 }
@@ -119,4 +119,24 @@ getBlastMatchesLocal <- function(DNAStringSetQuery, db) {
           DNAStringSetQuery,
           BLAST_args = "-num_threads 4") %>%
     return()
+}
+
+getDNAStringSetFromDataFrame <- function(dataframe, Genome) {
+  strings <- DNAStringSet()
+  for(i in 1:length(dataframe[,1])) {
+    strings <- c(strings, DNAStringSet(Genome[Genome@ranges@NAMES == dataframe$seqnames[i]][[1]][dataframe$start[i]:dataframe$end[i]]))
+  }
+  
+  return(DNAStringSet(strings))
+}
+
+getBlastMatchesOnline <- function(potentialPacks, db = "nt", Genome) {
+  packStrings <- getDNAStringSetFromDataFrame(potentialPacks, Genome)
+  
+  return(blastSeq(packStrings,
+                  n_blast = length(packStrings),
+                  delay_req = 20,
+                  database="nt",
+                  keepInMemory = TRUE,
+                  email = "jackgisby@gmail.com"))
 }
