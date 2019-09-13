10c673f267d0c401a5939cfd9c9563bf653493ee
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-13 21:53:00 +0100

    Scatter plot function development and investigation

--- R/clusterFunctions.R
+++ R/clusterFunctions.R
@@ -1,20 +1,62 @@
-getTirClusters <- function(potentialPacks, model = "TN93") {
+getTirHClust <- function(potentialPacks, model = "K80") {
   # gets clusters from the TIRs in potentialPacks and the model specified by user (see ape::dist.dna)
   
-  TIRs <- DNAStringSet(c(potentialPacks$forward_TIR, potentialPacks$reverse_TIR))
+  TIRs <- DNAStringSet(c(as.character(potentialPacks$forward_TIR), as.character(potentialPacks$reverse_TIR)))
   TIRs@ranges@NAMES <- c(paste0("f", row.names(potentialPacks)), paste0("r", row.names(potentialPacks)))
   
   dend <- as.DNAbin(TIRs) %>%
     dist.dna(model = model) %>% 
-    njs() %>%
+    hclust() %>%
     as.dendrogram() %>%
-    highlight_branches_col() %>%
-    hang.dendrogram(hang = -1) %>%
-    sort(type="nodes")
+    highlight_branches_col()
+
+  #idCols <- mapply(function(x) {return(as.integer(substr(x, 2, 3)))}, labels(dend), SIMPLIFY = TRUE)
+  dirCols <- ifelse(grepl("f", labels(dend)), 3, 4)
+  labels_colors(dend) <- dirCols
+  organismCols <- mapply(function(name, uniqueNames) {
+    return(which(uniqueNames == name) + 2)},
+    organism,
+    MoreArgs = list(unique(organism)))
+  
+
+  png("Data/Output/Plots/TIR_Relationships.png", width = 1000, height = 500)
+  plot(dend, main = "TIR Relationships")
+  colored_bars(colors = dirCols, dend=dend, sort_by_labels_order = FALSE, rowLabels = "direction")
+  dev.off()
+
+  plot(dend, main = "TIR Relationships")
+  colored_bars(colors = cbind(dirCols, organismCols), dend=dend, sort_by_labels_order = FALSE, rowLabels = c("direction", "organism"))
+
+  return(cbind(dirCols, organismCols))
+}
+
+getOrganismHClust <- function(potentialPacks, genomeList, model = "K80") {
+  # gets clusters from the TIRs in potentialPacks and the model specified by user (see ape::dist.dna)
+  # requires additional info on the genome of origin
+  
+  potentialPacks$Genome <- mapply(function(name, uniqueNames) {
+    return(which(uniqueNames == name) + 4)},
+    genomeList,
+    MoreArgs = list(unique(genomeList)))
+  TIRs <- DNAStringSet(c(as.character(potentialPacks$forward_TIR), as.character(potentialPacks$reverse_TIR)))
+  TIRs@ranges@NAMES <- c(paste0(potentialPacks$Genome, "f", row.names(potentialPacks)), paste0(potentialPacks$Genome, "r", row.names(potentialPacks)))
+
+  clust <- as.DNAbin(TIRs) %>%
+    #dist.dna(model = model)
+    kdistance(k=7)
+  clust[is.na(clust)] <- 0
+  clust[is.nan(clust)] <- 0
+  clust[is.infinite(clust)] <- 1
+  
+  dend <- clust %>%
+    hclust() %>%
+    as.dendrogram() %>%
+    highlight_branches_col()
   
   #idCols <- mapply(function(x) {return(as.integer(substr(x, 2, 3)))}, labels(dend), SIMPLIFY = TRUE)
   dirCols <- ifelse(grepl("f", labels(dend)), 3, 4)
   labels_colors(dend) <- dirCols
+  organismCols <- lapply(labels(dend), function(x) subseq(x, start=1, end=1))
   
   png("Data/Output/Plots/TIR_Relationships.png", width = 1000, height = 500)
   plot(dend, main = "TIR Relationships")
@@ -22,7 +64,39 @@ getTirClusters <- function(potentialPacks, model = "TN93") {
   dev.off()
   
   plot(dend, main = "TIR Relationships")
-  colored_bars(colors = dirCols, dend=dend, sort_by_labels_order = FALSE, rowLabels = "direction")
+  colored_bars(colors = organismCols, dend=dend, sort_by_labels_order = FALSE, rowLabels = "organism")
+  legend("topright", legend = unique(genomeList), fill = 5:(length(unique(genomeList))+5))
   
-  return(dend)
+  return(clust)
 }
+
+getOrganismKClust <- function(potentialPacks, genomeList, model = "K80") {
+  # gets clusters from the TIRs in potentialPacks and the model specified by user (see ape::dist.dna)
+  # requires additional info on the genome of origin
+  
+  potentialPacks$Genome <- mapply(function(name, uniqueNames) {
+    return(which(uniqueNames == name) + 4)},
+    genomeList,
+    MoreArgs = list(unique(genomeList)))
+  TIRs <- DNAStringSet(c(as.character(potentialPacks$forward_TIR), as.character(potentialPacks$reverse_TIR)))
+  TIRs@ranges@NAMES <- c(paste0("f", row.names(potentialPacks)), paste0("r", row.names(potentialPacks)))
+  
+  clust <- as.DNAbin(TIRs) %>%
+    #dist.dna(model = model)
+    kdistance()
+  
+  cmdscale(clust, eig=TRUE) %>%
+    fortify() %>%
+    setNames(c("MDS1", "MDS2")) %>%
+    ggplot(aes(MDS1, MDS2, colour = as.character(rep(potentialPacks$Genome, 2)),
+               label = TIRs@ranges@NAMES)) +
+    geom_point() + 
+    # geom_text(aes(label = TIRs@ranges@NAMES), position = position_nudge(x = 0.02), colour = "black") +
+    # scale_color_manual(labels = "black") +
+    scale_colour_discrete(name = "Organism", labels = unlist(unique(genomeList))) %>%
+    return()
+}
+
+
+
+  
--- testing.R
+++ testing.R
@@ -1,27 +1,52 @@
 source("packages.R")
+# 
+# genomeName <- c("Arabidopsis thaliana", "Arabidopsis lyrata", "Arabidopsis halleri", "Brassica rapa")
+# db <- c("refseq", "refseq", "genbank", "refseq")
+# 
+# #get genome
+# i <- 4
+# Genome <- getGenomeDnaStringSet(genomeName = genomeName[i], db = db[i])
+# 
+# 
+# #find packs
+# assessPotentialPackList(subSeqs = DNAStringSet(c("1" = "CACTACAA-AAATAT",
+#                                                  "2" = "CACTACAA-AAATAT",
+#                                                  "1" = "CACTACAA-AAATA",
+#                                                  "1" = "CACTACAA-AAA",
+#                                                  "0" = "CACTACAA")),
+#                         Genome = Genome,
+#                         element.length = c(300, 3500),
+#                         TSD.length = 3)
 
-genomeName <- c("Arabidopsis thaliana", "Arabidopsis lyrata", "Arabidopsis halleri", "Brassica rapa")
-db <- c("refseq", "refseq", "genbank", "refseq")
+Genome <- getGenomeDnaStringSet()
+knownCACTA <- getArathCACTA(Genome)
 
-#get genome
-i <- 4
-Genome <- getGenomeDnaStringSet(genomeName = genomeName[i], db = db[i])
+#get previously generated data
+getFiles <- function() {
+  files <- vector("list", length(list.files("Data/Output/algorithmAssessment/")))
+  
+  for(file in 1:length(list.files("Data/Output/algorithmAssessment/"))) {
+    files[[file]] <- read.csv(paste0("Data/Output/algorithmAssessment/",
+                                     list.files("Data/Output/algorithmAssessment/")[file],
+                                     "/potentialPacks.csv"))
+    
+    files[[file]]$Genome <- list.files("Data/Output/algorithmAssessment/")[file]
+    
+  }
+  
+  rbind(files[[1]],
+        files[[2]],
+        files[[3]],
+        files[[4]]) %>%
+    filter(TSD != "NNN") %>%
+    return()
+}
 
-
-#find packs
-assessPotentialPackList(subSeqs = DNAStringSet(c("1" = "CACTACAA-AAATAT",
-                                                 "2" = "CACTACAA-AAATAT",
-                                                 "1" = "CACTACAA-AAATA",
-                                                 "1" = "CACTACAA-AAA",
-                                                 "0" = "CACTACAA")),
-                        Genome = Genome,
-                        element.length = c(300, 3500),
-                        TSD.length = 3)
+potentialPacks <- getFiles()
 
 #clustering
-potentialPacks <- read.csv("Data/Output/algorithmAssessment/potentialPacks.csv") %>%
+clust <- potentialPacks %>% 
   filter(stringID == 5) %>%
-  mutate(forward_TIR = as.character(forward_TIR)) %>%
-  mutate(reverse_TIR = as.character(reverse_TIR))
-
-getTirClusters(potentialPacks, model = "TN93")
+  filter(Genome == unique(Genome)[4]) %>%
+  getOrganismKClust(.$Genome) %>%
+  print()
