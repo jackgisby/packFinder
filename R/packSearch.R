bea62a7f0ecfa71e31af49a603254e58884d640a
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-19 05:52:12 +0100

    Implemented VSEARCH clustering

--- R/packSearch.R
+++ R/packSearch.R
@@ -1,11 +1,11 @@
-packSearch <- function(subSeq, Genome, mismatch = 0, element.length, TSD.length) {
+packSearch <- function(subSeq, Genome, mismatch = 0, elementLength, tsdLength) {
   # General use pipeline function for the Pack-TYPE transposon finding algorithm
   #
   # ---input---
   # subSeq: DNAString object containing the TIR sequence
   # Genome: DNAStringSet object to be searched
   # mismatch: the maximum edit distance to be considered for TIR matches (indels + substitions)
-  # element.length: vector containing the minimum and maximum length of the Pack-TYPE transposons
+  # elementLength: vector containing the minimum and maximum length of the Pack-TYPE transposons
   #   
   # ---returns---
   # a dataframe of potential Pack-TYPE transposons
@@ -14,12 +14,12 @@ packSearch <- function(subSeq, Genome, mismatch = 0, element.length, TSD.length)
   print("Getting forward matches")
   forwardMatches <- data.frame(seqnames = character(), start = integer(), end = integer(), width = integer(), strand = character()) %>%
     identifyTIRMatches(subSeq, Genome, mismatch = mismatch, strand = "+") %>%
-    getTSDs(Genome, TSD.length, direction = "+")
+    getTSDs(Genome, tsdLength, direction = "+")
   
   print("Getting reverse matches")
   reverseMatches <- data.frame(seqnames = character(), start = integer(), end = integer(), width = integer(), strand = character()) %>%
     identifyTIRMatches(reverseComplement(subSeq), Genome, mismatch = mismatch, strand = "-") %>%
-    getTSDs(Genome, TSD.length, direction = "-")
+    getTSDs(Genome, tsdLength, direction = "-")
   
   #case: no matches
   if(length(forwardMatches[,1]) == 0 | length(reverseMatches[,1]) == 0) {
@@ -28,21 +28,10 @@ packSearch <- function(subSeq, Genome, mismatch = 0, element.length, TSD.length)
   
   #determine potential transposable elements based on nearby elements and TSD sequences
   print("Filtering matches based on TSD sequences")
-  potentialPacks <- identifyPotentialPackElements(forwardMatches, reverseMatches, Genome, element.length) %>%
-    getTSDs(Genome, TSD.length, "+") %>%
-    getTIRs(Genome)
+  packMatches <- identifyPotentialPackElements(forwardMatches, reverseMatches, Genome, elementLength) %>%
+    getTSDs(Genome, tsdLength, "+") %>%
+    getSeqs(Genome)
   
   print("Initial filtering complete")
-  return(potentialPacks)
-}
-
-packBlast <- function(potentialPacks, db, db.loc = "local", Genome) {
-  blastMatches <- getBlastMatches(potentialPacks, db, db.loc) %>%
-    return()
-}
-
-packPipeline <- function(subSeq, Genome, mismatch = 0, element.length, TSD.length, db.loc = "online") {
-  packSearch(subSeq, Genome, mismatch, element.length, TSD.length) %>%
-    packBlast() %>%
-    return()
+  return(packMatches)
 }
