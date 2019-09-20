5bb6a387638d0eafa41d0c6a129abd5507ed4b7d
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-20 23:39:17 +0100

    Added Arabidopsis thaliana dataset as example.

---
+++ R/arabidopsisThalianaRefseq.R
@@ -0,0 +1,6 @@
+#' @title Arabidopsis thaliana Refseq Genome
+#' @description The Arabidopsis thaliana refseq genome, downloaded using \code{\link[biomartr]{getGenome}}
+#' @docType data
+#' @usage data(arabidopsisThalianaRefseq)
+
+"arabidopsisThalianaRefseq.RData"
