582661424fe825c351b5968ac575b1c721c1b6ab
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-09-20 19:25:11 +0100

    Updating package documentation.
    Former-commit-id: 14f32ed228cab53b069fd01a85100130ec3ab8dd [formerly b8b4a78296764df895ead0d1002b600574d35bd7]
    Former-commit-id: cd095e03f9d8f508fb5969b4218c8c0a4bbb46fd

--- R/getSeqs.R
+++ R/getSeqs.R
@@ -1,15 +1,17 @@
-#' Gets the sequences referred to in \code{packMatches} and returns the sequences added as an additional column to the dataframe..
-#'
+#' @title Get Sequences from Ranges.
+#' @description Gets the sequences referred to in \code{packMatches} and returns the sequences added as an additional column to the dataframe..
 #' @param packMatches A dataframe containing genomic ranges and names referring to sequences to be extracted.
 #' @param Genome A DNAStringSet object containing sequences referred to in \code{packMatches}
+#' @author Jack Gisby
+#' @details
+#' Function intended for internal use.
 #' @return The dataframe \code{packMatches} with an additional \code{seq} feature containing extracted sequences as characters.
-#' @export
 
 getSeqs <- function(packMatches,
                     Genome) {
 
   packMatches %>%
-    mutate(seq = mapply(function(start, end, seqnames, Genome) {
+    dplyr::mutate(seq = mapply(function(start, end, seqnames, Genome) {
       return(as.character(Genome[Genome@ranges@NAMES == seqnames][[1]][start:end]))},
       start,
       end,
