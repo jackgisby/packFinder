865a06c2eab7c4626ea7377fff9e9e8e06599831
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2019-10-12 23:48:56 +0100

    Vignette update and cluster savefolder fix
    Former-commit-id: 9a2870ee5a288e746e85794d2418d279b3127445 [formerly d821eea933f426be6f9fcfde1f340581d3ff8de0]
    Former-commit-id: 2d2a8168f482e7ab1c141d88f89756c36b1f0181

--- tests/testthat/test-Clust.R
+++ tests/testthat/test-Clust.R
@@ -7,8 +7,8 @@ data("arabidopsisThalianaRefseq")
 vSearchLocation <- "E:/vsearch-2.14.1-win-x86_64/vsearch.exe"
 
 if(!is.null(vSearchLocation)) {
-  packClusts <- packClust(packMatches, arabidopsisThalianaRefseq, saveFolder = "data-raw/output/", vSearchPath = vSearchLocation)
-  packAlign <- packAlign(packMatches, arabidopsisThalianaRefseq, saveFolder = "data-raw/output/", vSearchPath = vSearchLocation)
+  packClusts <- packClust(packMatches, arabidopsisThalianaRefseq, saveFolder = "data-raw/output", vSearchPath = vSearchLocation)
+  packAlign <- packAlign(packMatches, arabidopsisThalianaRefseq, saveFolder = "data-raw/output", vSearchPath = vSearchLocation)
 }
 
 test_that("Clusters identified are as expected", {
