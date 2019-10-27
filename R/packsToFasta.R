5e5bc2a4af9efdabf433a919d54518f4e156ccd3
jackgisby <43494691+jackgisby@users.noreply.github.com>
2019-10-27 16:48:31 +0000

    Re-formatted documentation and fixed clustering bug
    Former-commit-id: 721144d5fe77fbab2d1fbe1e8394c34f76c8dfad [formerly 06f6d42f0cdba3658d09f1c4df4c2f648ef2093b]
    Former-commit-id: 22d20c64e10183fe0393b191cb3a7a7882bd5454

--- R/packsToFasta.R
+++ R/packsToFasta.R
@@ -1,56 +1,50 @@
 #' @title
-#'     Save packFinder Results in FASTA Format (.fasta)
+#' Save packFinder Results in FASTA Format (.fasta)
 #'
 #' @description
-#'     Saves a dataframe of potential Pack-TYPE 
-#'     elements, usually generated via 
-#'     \code{\link{packSearch}}. May be retrieved 
-#'     using \code{\link{getPacksFromFasta}}.
+#' Saves a dataframe of potential Pack-TYPE elements, 
+#' usually generated via \code{\link{packSearch}}. 
+#' May be retrieved using \code{\link{getPacksFromFasta}}.
 #'
 #' @param file
-#'     FASTA file save path.
+#' FASTA file save path.
 #'
 #' @param packMatches
-#'     A dataframe containing genomic ranges and 
-#'     names referring to sequences to be extracted. 
-#'     Can be obtained from \code{\link{packSearch}} 
-#'     or generated from a 
-#'     \code{\link[GenomicRanges:GRanges-class]{GRanges}} 
-#'     object, afterconversion to a dataframe. 
-#'     Must contain the following features:
-#'     \itemize{
-#'         \item start - the predicted element's 
-#'             start base sequence position.
-#'         \item end - the predicted element's end 
-#'             base sequence position.
-#'         \item seqnames - character string 
-#'             referring to the sequence name in
-#'         \code{Genome} to which \code{start} 
-#'             and \code{end} refer to.
+#' taframe containing genomic ranges and names referring 
+#' to sequences to be extracted. Can be obtained from 
+#' \code{\link{packSearch}} or generated from a 
+#' \code{\link[GenomicRanges:GRanges-class]{GRanges}} object, 
+#' after conversion to a dataframe. Must contain the 
+#' following features:
+#' \itemize{
+#'     \item start - the predicted element's start base 
+#'     sequence position.
+#'     \item end - the predicted element's end 
+#'     base sequence position.
+#'     \item seqnames - character string 
+#'     referring to the sequence name in \code{Genome} to 
+#'     which \code{start} and \code{end} refer to.
 #' }
 #'
 #' @param Genome
-#'     A DNAStringSet object containing sequences 
-#'     referred to in \code{packMatches} (the object 
-#'     originally used to predict the transposons 
-#'     \code{\link{packSearch}}).
-#'
-
+#' A DNAStringSet object containing sequences referred to 
+#' in \code{packMatches} (the object originally used to 
+#' predict the transposons \code{\link{packSearch}}).
 #'
 #' @return
-#'     Save location of Fasta file.
-#'
-#' @author
-#'     Jack Gisby
+#' Save location of Fasta file.
 #'
 #' @seealso 
-#'     \code{\link{getPacksFromFasta}}
+#' \code{\link{getPacksFromFasta}}
+#' 
+#' @author
+#' Jack Gisby
 #' 
 #' @examples
-#'     data(arabidopsisThalianaRefseq)
-#'     data(packMatches)
+#' data(arabidopsisThalianaRefseq)
+#' data(packMatches)
 #' 
-#'     packsToFasta(packMatches, "packMatches.fa", arabidopsisThalianaRefseq)
+#' packsToFasta(packMatches, "packMatches.fa", arabidopsisThalianaRefseq)
 #'
 #' @export
 
