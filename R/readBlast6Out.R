905b725a1ae223e9f293d09cef3c426e0a480607
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2019-12-05 18:22:45 +0000

    Removed double read permission

--- R/readBlast6Out.R
+++ R/readBlast6Out.R
@@ -58,7 +58,6 @@
 readBlast6Out <- function(file) {
     if (!is.null(file)) {
         if (!(file.access(file, 4) == 0) |
-            !(file.access(file, 4) == 0) |
             !(file.access(file, 2) == 0)) {
             stop("file does not exist, or R 
                 does not have read/write permissions")
